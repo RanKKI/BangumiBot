edaf2ce4844e21671d3a8c541ccbd63f51d281a0
RanKKI <youloveonlymeh@gmail.com>
2022-07-09 20:21:24 +0800

    fix: download folder

--- bangumi/util/files.py
+++ bangumi/util/files.py
@@ -6,8 +6,12 @@ from bangumi.util.const import Env
 
 def move_file(file: Path, folder_name: str, new_file_name: str) -> None:
     media_folder = Path(os.environ.get(Env.MEDIA_FOLDER.value, "media"))
+    download_folder = Path(os.environ.get(Env.DOWNLOAD_FOLDER.value, "downloads"))
     ext = os.path.splitext(file.name)[1]
     target_folder = media_folder / folder_name
     target_folder.mkdir(parents=True, exist_ok=True)
     target_file = target_folder / f"{new_file_name}{ext}"
-    file.rename(target_file)
+
+    abs_download_file = download_folder / file.name
+
+    abs_download_file.rename(target_file)

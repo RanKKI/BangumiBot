3307c05b4532a99c1f1f792708e5d25f804e4b2f
RanKKI <RanKKI@users.noreply.github.com>
2022-07-15 01:38:01 +0000

    style: auto fixes

--- bangumi/util/datetime.py
+++ bangumi/util/datetime.py
@@ -1,12 +1,12 @@
-
 from datetime import datetime
 
 
 POSSIBLE_FORMAT = [
     "%Y-%m-%dT%H:%M:%S.%f",
     "%Y-%m-%dT%H:%M:%S",
-    "%a, %d %b %Y %H:%M:%S %z"
+    "%a, %d %b %Y %H:%M:%S %z",
 ]
+
 
 def get_timestamp(val: str) -> int:
     for format in POSSIBLE_FORMAT:

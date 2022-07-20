5c5cdadecfffcd40372c2245f7a1edc79c222e68
RanKKI <youloveonlymeh@gmail.com>
2022-07-20 16:02:40 +0800

    fix: rss regex rule error

--- bangumi/rss/rss.py
+++ bangumi/rss/rss.py
@@ -37,7 +37,9 @@ class RSS(object):
         if not os.path.exists(config_path):
             return
         with open(config_path, "r") as f:
-            data = json.load(f)
+            # using .encode('unicode_escape') to avoid regular expression error
+            data = json.load(f.encode('unicode_escape'))
+
         for url in data.get("urls", []):
             site: RSSSite = None
             if isinstance(url, str):

7e2ac67fed1e902bcea2d686892c7a2a9f9cfd2a
DDSRem <1448139087@qq.com>
2022-11-27 15:44:28 +0800

    Optimize permission settings

--- start.sh
+++ start.sh
@@ -7,9 +7,17 @@ chown -R ${PUID}:${PGID} \
     /src \
     /config
 
-chown ${PUID}:${PGID} \
-    /cache \
-    /media \
-    /downloads
+if [[ "$(stat -c '%u' /cache)" != "${PUID}" ]] || [[ "$(stat -c '%g' /cache)" != "${PGID}" ]]; then
+    chown ${PUID}:${PGID} \
+        /cache
+fi
+if [[ "$(stat -c '%u' /media)" != "${PUID}" ]] || [[ "$(stat -c '%g' /media)" != "${PGID}" ]]; then
+    chown ${PUID}:${PGID} \
+        /media
+fi
+if [[ "$(stat -c '%u' /downloads)" != "${PUID}" ]] || [[ "$(stat -c '%g' /downloads)" != "${PGID}" ]]; then
+    chown ${PUID}:${PGID} \
+        /downloads
+fi
 
 su-exec ${PUID}:${PGID} uvicorn main:app --host 0.0.0.0 --log-config conf/log.yml

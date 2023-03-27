6b4a456d5f5ed7ff92f5d6fdd8fe59207b8c42f5
RanKKI <youloveonlymeh@gmail.com>
2023-03-27 21:33:17 +1100

    fix: no config in api call

--- bangumi/api/rss.py
+++ bangumi/api/rss.py
@@ -1,7 +1,9 @@
+from pydantic import BaseModel
+
 from bangumi.database import redisDB
 from bangumi.entitiy import RSSSite
+from bangumi.manager import ConfigManager
 from bangumi.rss import RSS
-from pydantic import BaseModel
 
 from .app import app as api
 
@@ -12,6 +14,11 @@ class AddRss(BaseModel):
 
 @api.post("/add_by_rss")
 async def add_torrents_by_rss(r: AddRss):
-    items = await RSS().scrape_url(RSSSite(url=r.url))
-    redisDB.add_to_torrent_queue(items)
+    config = ConfigManager()
+    rss = RSS()
+    config.register("rss.json", rss)
+    config.load_config()
+
+    items = await rss.scrape_url(RSSSite(url=r.url))
+    # redisDB.add_to_torrent_queue(items)
     return {"message": "OK!", "items": items}
